7df5c7e9afb8a1eb86b8ab33503a67ad1ff847ab
victorlima11 <victorconta1009@gmail.com>
2024-12-16 18:43:53 -0300

    sduisdgfyusdyufs

--- meu_app/models.py
+++ meu_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.nome
+    
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    quantidade = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+    
+class Pedido(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Pedido de {self.cliente.nome} - Produto: {self.produto.nome}"
